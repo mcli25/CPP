fae65539c2f161f47269201bce7044e40b589630
lee <mcli2023@outlook.com>
2024-02-05 21:40:07 +0000

    lab6

--- week06/examples/lab/main.cpp
+++ week06/examples/lab/main.cpp
@@ -4,7 +4,7 @@
 #include "mymath.h"
 int main()
 {
-    float arr1[8]{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f};
+    float arr1[8] = {1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f};
     float * arr2 = NULL;
 
     float sum1 = arraySum(arr1, 8);
